e03538c05255c3bba6be1fbbaf807fcbe06d1c83
Ruta Butkute <rut.butkute@gmail.com>
2013-10-10 14:56:31 +0300

    ND1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <?php
 
     //if they DID upload a file...
-    if($_FILES['photo']['name'])
+    if(!empty($_FILES['photo']['name']))
     {
     //if no errors...
     if(!$_FILES['photo']['error'])
@@ -22,19 +22,20 @@
        $extension = end($temp);
        //now is the time to modify the future file name and validate the file
        $new_file_name = time() . '.' .  $extension; //rename file
-    if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
-    {
-       $valid_file = false;
-       echo 'Oops!  Your file\'s size is to large.';
-    }
+        if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
+        {
+           $valid_file = false;
+           echo 'Oops!  Your file\'s size is to large.';
+        }
 
          //if the file has passed the test
-    if($valid_file)
-    {
-            //move it to where we want it to be
-            move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
-            echo 'Congratulations!  Your file was accepted.';
-    }
+        if($valid_file)
+        {
+                //move it to where we want it to be
+                move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
+                echo 'Congratulations!  Your file was accepted.<br>';
+                echo '<img src="uploads/' . $new_file_name . '" width="600">';
+        }
     }
     //if there is an error...
     else
@@ -43,6 +44,5 @@
         echo 'Ooops!  Your upload triggered the following error:  '.$_FILES['photo']['error'];
 
     }
-    echo '<img src="uploads/' . $new_file_name . '">';
-    }
 
+    }
